35301c022e9def6ff3554d4ec28d961a2516f756
Thajmil akthar <thajmiloffical@gmail.com>
2026-05-30 11:35:00 +0530

    Add files via upload

---
+++ trio-products-website/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Thank you for contacting Trio Products!");
+});
